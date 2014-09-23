f8045aaf8100534ceb19ce10263ce8f79a03785e
Silvia Taroni <Silvia.Taroni@cern.ch>
2014-09-23 03:49:04 -0500

    update

--- lfvetau/make_proxies.sh
+++ lfvetau/make_proxies.sh
@@ -18,8 +18,8 @@ fi
 echo "Building cython wrappers from file: $afile"
 
 rake "make_wrapper[$afile, ee/final/Ntuple, EETree]"
-#rake "make_wrapper[$afile, eet/final/Ntuple, EETauTree]"
-#rake "make_wrapper[$afile, et/final/Ntuple, ETauTree]"
+rake "make_wrapper[$afile, eet/final/Ntuple, EETauTree]"
+rake "make_wrapper[$afile, et/final/Ntuple, ETauTree]"
 ls *pyx | sed "s|pyx|so|" | xargs -n 1 -P 10 rake 
 
 #export jobid=$jobidmt
