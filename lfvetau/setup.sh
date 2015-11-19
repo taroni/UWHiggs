0a16976c02617fbcce87c625f2323d9825cad6f2
Silvia Taroni <Silvia.Taroni@cern.ch>
2015-11-19 04:49:13 -0600

    ongoing

--- lfvetau/setup.sh
+++ lfvetau/setup.sh
@@ -8,11 +8,11 @@ export jobid=$jobid13
 
 echo $jobid
 #export datasrc=/hdfs/store/user/$USER/  #$(ls -d /scratch/*/data/$jobid | awk -F$jobid '{print $1}')
-export datasrc=/hdfs/store/user/ndev/  #$(ls -d /scratch/*/data/$jobid | awk -F$jobid '{print $1}')
-export MEGAPATH=/hdfs/store/user/ndev/
+export datasrc=/hdfs/store/user/cepeda/  #$(ls -d /scratch/*/data/$jobid | awk -F$jobid '{print $1}')
+export MEGAPATH=/hdfs/store/user/cepeda/
 #./make_proxies.sh
-#rake "meta:getinputs[$jobid, $datasrc,emm/metaInfo, emm/summedWeights]"
-rake "meta:getmeta[inputs/$jobid, emm/metaInfo, 13, emm/summedWeights]"
+#rake "meta:getinputs[$jobid, $datasrc,et/metaInfo, et/summedWeights]"
+rake "meta:getmeta[inputs/$jobid, et/metaInfo, 13, et/summedWeights]"
 
 
 #rake "meta:getinputs[$jobid, $datasrc,ee/metaInfo]"
